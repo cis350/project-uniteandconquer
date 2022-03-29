5bcd155159fb5d9779ba0354619f0d392554467c
linda-XI <yuxidai@seas.upenn.edu>
2022-03-29 00:49:20 -0400

    add middleware for profile page
    1. get info from database. "tags" can be used in sideBar.js
    
    2. front end table need to be modified. I create a helper function: PostTableGenerator, which may be helpful to render the table

--- uniteandconquer/src/components/UserProfile.js
+++ uniteandconquer/src/components/UserProfile.js
@@ -1,10 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SidebarTags from './SidebarTags';
 import Notifications from './Notifications';
 import '../assets/UserProfile.css';
 
+const UserDB = require('../modules/UserDB');
+
 function UserProfile() {
   const [showNotifs, setShowNotifs] = useState(false);
+  /** used for showing interest in side bar */
+  // const [tags, setTags] = useState([]);
+  /** list of posts and whish list that will show on profile page */
+  const [posts, setPosts] = useState([]);
+  const [whishList, setWishList] = useState([]);
+
+  const myStorage = window.sessionStorage;
+  const PostTableGenerator = (postsOrWishList) => {
+    const table = postsOrWishList.map((post) => (
+      <td>
+        <div className="post-title">
+          Post [
+          {post.id}
+          ]: [
+          {post.itemName}
+          ] - [
+          {post.status}
+          ]
+        </div>
+        <div className="post-content">
+          This post is led by [
+          {post.ownerName}
+          ] and trades [
+          {post.itemNumTarget}
+          ] of [
+          {post.itemNumCurrent}
+          ] for
+          $[
+          {post.pricePerItem}
+          ]
+          <br />
+          <br />
+          [
+          {post.tags}
+          ]
+        </div>
+      </td>
+    ));
+    return table;
+  };
+
+  useEffect(() => {
+    const userID = myStorage.getItem('UserID');
+    UserDB.getUserDetails(userID, (success, userInfo, err) => {
+      if (success) {
+        // setTags(userInfo.interests);
+        setPosts(userInfo.posts);
+        // setWishList(userInfo.wishList);
+      } else {
+        console.log(err);
+      }
+    });
+  }, []);
 
   return (
     <div className="user-profile-page">
@@ -26,6 +81,7 @@ function UserProfile() {
                 <th>My Wishlist</th>
               </tr>
               <tr>
+                {/* {PostTableGenerator(posts)} */}
                 <td>
                   <div className="post-title">Post [ID]: [itemName] - [status]</div>
                   <div className="post-content">
