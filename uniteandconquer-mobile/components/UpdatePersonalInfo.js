ed4760e517200e9938f676fb2d7793a63457bd22
Zhihang <zhyuan1@seas.upenn.edu>
2022-04-03 16:03:56 -0400

    Update UpdatePersonalInfo.js
    Add db calls

--- uniteandconquer-mobile/components/UpdatePersonalInfo.js
+++ uniteandconquer-mobile/components/UpdatePersonalInfo.js
@@ -4,6 +4,8 @@ import {
 } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
 
+const userDB = require('../modules/UserDB');
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FAE9C7',
@@ -64,19 +66,57 @@ const styles = StyleSheet.create({
 });
 
 function UpdateInfo({ navigation }) {
-  const [username, setUsername] = useState(null);
-  const [fullName, setFullName] = useState(null);
+  // const [username, setUsername] = useState(null);
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
   const [phone, setPhone] = useState(null);
   const [email, setEmail] = useState(null);
   const [countryCode, setCountryCode] = useState(null);
-
   const countryCodeList = ['+1', '+86'];
+
+  /**
+   * how to retrieve the user id is to be decided.
+   */
+  const userid = 'TBD';
+  /**
+   *
+   * @param {*} input the input could be one of the user input information
+   * @returns true if the input is not initialized or is empty
+   */
+  function checkValidInput(input) {
+    return (input != null && input.trim().length >= 1);
+  }
+
+  /**
+   * update the personal information if the input is valid
+   */
   function handleUpdate() {
-    console.log('The username is ', username);
-    console.log('The fullName is ', fullName);
-    console.log('The phone is ', phone);
-    console.log('The email is ', email);
-    console.log('The country code is ', countryCode);
+    // Update first name
+    if (checkValidInput(firstName)) {
+      userDB.modifyUser(userid, 3, firstName, null, (success, error) => {
+        console.log(error);
+      });
+    }
+    // Update last name
+    if (checkValidInput(lastName)) {
+      userDB.modifyUser(userid, 4, lastName, null, (success, error) => {
+        console.log(error);
+      });
+    }
+    // Update phone
+    if (checkValidInput(phone) && checkValidInput(countryCode)) {
+      const newValue = { countryCode, phone };
+      userDB.modifyUser(userid, 0, newValue, null, (success, error) => {
+        console.log(error);
+      });
+    }
+
+    // Update phone
+    if (checkValidInput(email)) {
+      userDB.modifyUser(userid, 1, email, null, (success, error) => {
+        console.log(error);
+      });
+    }
   }
 
   return (
@@ -88,7 +128,7 @@ function UpdateInfo({ navigation }) {
       </View>
 
       <View style={styles.inputContainer}>
-        <View style={styles.subtitle}>
+        {/* <View style={styles.subtitle}>
           <Text>Username</Text>
         </View>
         <View style={styles.textInput}>
@@ -97,16 +137,27 @@ function UpdateInfo({ navigation }) {
             onChangeText={setUsername}
             value={username}
           />
-        </View>
+        </View> */}
 
         <View style={styles.subtitle}>
-          <Text>Full Name</Text>
+          <Text>First Name</Text>
         </View>
         <View style={styles.textInput}>
           <TextInput
-            placeholder="Full Name"
-            onChangeText={setFullName}
-            value={fullName}
+            placeholder="First Name"
+            onChangeText={setFirstName}
+            value={firstName}
+          />
+        </View>
+
+        <View style={styles.subtitle}>
+          <Text>Last Name</Text>
+        </View>
+        <View style={styles.textInput}>
+          <TextInput
+            placeholder="Last Name"
+            onChangeText={setLastName}
+            value={lastName}
           />
         </View>
 
