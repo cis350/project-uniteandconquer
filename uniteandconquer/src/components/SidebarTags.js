7f748883b2c898901fcf7c795535c5c1dd71e133
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2022-04-16 17:12:29 -0400

    fixed styling user profile + sidebar + post details

--- uniteandconquer/src/components/SidebarTags.js
+++ uniteandconquer/src/components/SidebarTags.js
@@ -5,23 +5,23 @@ import '../assets/SidebarTags.css';
 function SidebarTags() {
   return (
     <div className="sidebar">
-      <div className="greeting">
+      <div className="greeting-box">
         <i className="far fa-user-circle fa-2x" />
         {' '}
         Hi, Jeremy
       </div>
 
-      <div className="home">
+      <div className="home-box">
         <Link className="link" to="/">
           <div className="text">Home</div>
         </Link>
       </div>
-      <div className="settings">
+      <div className="settings-box">
         <Link className="link" to="/user-settings">
           <div className="text">Settings</div>
         </Link>
       </div>
-      <div className="preferences">
+      <div className="preferences-box">
         <div className="preferences-title">My Preferences</div>
         <div className="preference-tag">Tag1</div>
         <div className="preference-tag">Tag2</div>
