a2aae0c28ef9d1ee79602b252a1b6337a03c3ff8
Zhihang <zhyuan1@seas.upenn.edu>
2022-05-09 00:36:04 -0700

    Update UserDB.js

--- uniteandconquer/src/modules/UserDB.js
+++ uniteandconquer/src/modules/UserDB.js
@@ -1,5 +1,7 @@
 /* User-related operations */
-function createUser(
+import axios from 'axios';
+const rootURL = 'http://localhost:8080';
+async function createUser(
   countryCode,
   phoneNumber,
   email,
@@ -9,57 +11,84 @@ function createUser(
   interests,
   callback,
 ) {
-  return callback(true, '507f1f77bcf86cd799439011', null);
+  const response = await axios.post(`${rootURL}/createUser`, {
+    phone: { countryCode: Number(countryCode), phoneNumber: Number(phoneNumber) },
+    email: `${email}`,
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
+    posts: null,
+    wishList: null,
+    interests: interests,
+    password: `${password}`,
+    createdAt: null,
+    lastCheckNotification: null,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
 
-function loginUserWithPhone(countryCode, phoneNumber, password, callback) {
-  return callback(true, '507f1f77bcf86cd799439011', null);
+async function loginUserWithPhone(
+  countryCode,
+  phoneNumber,
+  password,
+  callback,
+) {
+  const response = await axios.post(`${rootURL}/loginUserWithPhone`, {
+    phone: { countryCode: Number(countryCode), phoneNumber: Number(phoneNumber) },
+    password: `${password}`,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
 
-function loginUserWithEmail(email, password, callback) {
-  return callback(true, '507f1f77bcf86cd799439011', null);
+async function loginUserWithEmail(
+  email,
+  password,
+  callback,
+) {
+  const response = await axios.post(`${rootURL}/loginUserWithEmail`, {
+    email: `${email}`,
+    password: `${password}`,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
 
-function modifyUser(id, fieldToChange, newValue, oldPassword, callback) {
+async function modifyUser(id, fieldToChange, newValue, oldPassword, callback) {
   return callback(true, null);
 }
-function getPassword(id, callback) {
-  return callback(true, null);
+
+async function getPassword(
+  id,
+  callback,
+) {
+  const response = await axios.get(`${rootURL}/getPassword`, {
+    userId: id,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
 
-function getUserDetails(id, callback) {
-  return callback(true, {
-    phone: { countryCode: '1', phoneNumber: '9783999395' },
-    email: 'yuyingf@seas.upenn.edu',
-    firstName: 'Yuying',
-    lastName: 'Fan',
-    interests: ['Food', 'Home'],
-    posts: [{
-      id: '5087901e810c19729de860ea', itemName: 'AA Batteries', itemNumTarget: 20, itemNumCurrent: 5, pricePerItem: 0.46, ownerName: 'Yuying Fan', status: 0, tags: ['Home'],
-    }],
-    wishList: [{
-      id: '507f191e810c19729de860ea', itemName: 'Trash Can', itemNumTarget: 5, itemCurrent: 3, pricePerItem: 2.0, ownerName: 'Yuxi Dai', status: 0, tags: ['Home'],
-    },
-    {
-      id: '507f191e810c19729dccba45', itemName: 'Laundry Bags', itemNumTarget: 2, itemCurrent: 3, pricePerItem: 14.99, ownerName: 'Roy Bae', status: 1, tags: ['Home'],
-    }],
-  }, null);
+async function getUserDetails(
+  id,
+  callback,
+) {
+  const response = await axios.get(`${rootURL}/getUserDetails`, {
+    userId: id,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
-function getChats(id, callback) {
-  return callback(
-    true,
-    [{
-      id: '5087901e810c109679e860ea',
-      groupName: 'chat1',
-      hasUpdates: true,
-    },
-    {
-      id: '5087901e810c109679e860eb',
-      groupName: 'chat2',
-      hasUpdates: true,
-    }],
-    null,
-  );
+
+async function getChats(
+  id,
+  callback,
+) {
+  const response = await axios.get(`${rootURL}/getChats`, {
+    userId: id,
+  });
+  const result = response.data;
+  return callback(result.success, result.data, result.error);
 }
 
 export {
