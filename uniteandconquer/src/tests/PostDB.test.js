4b7023d7a9fdd256944e394e73dd9ac084e4bc51
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2022-05-10 16:46:54 -0700

    handled promise errors in db tests

--- uniteandconquer/src/tests/PostDB.test.js
+++ uniteandconquer/src/tests/PostDB.test.js
@@ -16,104 +16,132 @@ beforeAll(() => {
 
 test('addPost could add a new post', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/addPost`).reply(201, { success: true, data: 'PostID', error: null });
-  await api.addPost(
-    'apple',
-    25,
-    500,
-    20,
-    'apple.com',
-    'juicy',
-    'ObjectID',
-    ['food'],
-    (success, data, error) => {
-      expect(success).toBe(true);
-      expect(data).toBe('PostID');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/addPost`).reply(201, { success: true, data: 'PostID', error: null });
+    await api.addPost(
+      'apple',
+      25,
+      500,
+      20,
+      'apple.com',
+      'juicy',
+      'ObjectID',
+      ['food'],
+      (success, data, error) => {
+        expect(success).toBe(true);
+        expect(data).toBe('PostID');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('addComment could add a new comment', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/addComment`).reply(201, { success: true, error: null });
-  await api.addComment(
-    'ObjectID',
-    'PostID',
-    'content',
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/addComment`).reply(201, { success: true, error: null });
+    await api.addComment(
+      'ObjectID',
+      'PostID',
+      'content',
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('getPost could fetch a post', async () => {
   // mock the axios response- new user 201
-  mock.onGet(`${rootURL}/getPost/id`).reply(201, { success: true, data: 'post', error: null });
-  await api.getPost(
-    'id',
-    (success, data, error) => {
-      expect(success).toBe(true);
-      expect(data).toBe('post');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onGet(`${rootURL}/getPost/id`).reply(201, { success: true, data: 'post', error: null });
+    await api.getPost(
+      'id',
+      (success, data, error) => {
+        expect(success).toBe(true);
+        expect(data).toBe('post');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('getSortedPostBySearch could fetch posts based on search conditions', async () => {
   // mock the axios response- new user 201
-  mock.onGet(`${rootURL}/getSortedPostBySearch/startIdx/endIdx/?keywords=keywords&tags[]=tag`).reply(201, { success: true, data: 'post', error: null });
-  await api.getSortedPostBySearch(
-    'startIdx',
-    'endIdx',
-    'keywords',
-    ['tag'],
-    (success, data, error) => {
-      expect(success).toBe(true);
-      expect(data).toBe('post');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onGet(`${rootURL}/getSortedPostBySearch/startIdx/endIdx/?keywords=keywords&tags[]=tag`).reply(201, { success: true, data: 'post', error: null });
+    await api.getSortedPostBySearch(
+      'startIdx',
+      'endIdx',
+      'keywords',
+      ['tag'],
+      (success, data, error) => {
+        expect(success).toBe(true);
+        expect(data).toBe('post');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('joinGroup could joins a user in group', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/joinGroup`).reply(201, { success: true, error: null });
-  await api.joinGroup(
-    'UserId',
-    'PostId',
-    25,
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/joinGroup`).reply(201, { success: true, error: null });
+    await api.joinGroup(
+      'UserId',
+      'PostId',
+      25,
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('leaveGroup could leaves a user from group', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/leaveGroup`).reply(201, { success: true, error: null });
-  await api.leaveGroup(
-    'UserId',
-    'PostId',
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/leaveGroup`).reply(201, { success: true, error: null });
+    await api.leaveGroup(
+      'UserId',
+      'PostId',
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('changePostStatus could leaves a user from group', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/changePostStatus`).reply(201, { success: true, error: null });
-  await api.changePostStatus(
-    'UserId',
-    'PostId',
-    'newStatus',
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/changePostStatus`).reply(201, { success: true, error: null });
+    await api.changePostStatus(
+      'UserId',
+      'PostId',
+      'newStatus',
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
--- uniteandconquer/src/tests/UserDB.test.js
+++ uniteandconquer/src/tests/UserDB.test.js
@@ -16,87 +16,111 @@ beforeAll(() => {
 
 test('createUser creates a new player', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/createUser`).reply(201, { success: true, error: null });
-  await api.createUser(
-    '1',
-    '4452219194',
-    'zhyuan1@seas.upenn.edu',
-    'Zhihang311@',
-    'Zhihang',
-    'Yuan',
-    'food',
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/createUser`).reply(201, { success: true, error: null });
+    await api.createUser(
+      '1',
+      '4452219194',
+      'zhyuan1@seas.upenn.edu',
+      'Zhihang311@',
+      'Zhihang',
+      'Yuan',
+      'food',
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('loginUserWithPhone logins an user', async () => {
-  mock.onPost(`${rootURL}/loginUserWithPhone`).reply(201, { success: true, id: 'objectID', error: null });
-  await api.loginUserWithPhone(
-    '1',
-    '4452219194',
-    'Zhihang311@',
-    (success, id, error) => {
-      expect(success).toBe(true);
-      expect(id).toBe('objectID');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/loginUserWithPhone`).reply(201, { success: true, id: 'objectID', error: null });
+    await api.loginUserWithPhone(
+      '1',
+      '4452219194',
+      'Zhihang311@',
+      (success, id, error) => {
+        expect(success).toBe(true);
+        expect(id).toBe('objectID');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('loginUserWithEmail logins an user', async () => {
   // mock the axios response- new user 201
-  mock.onPost(`${rootURL}/loginUserWithEmail`).reply(201, { success: true, id: 'objectID', error: null });
-  await api.loginUserWithEmail(
-    'zhyuan1@seas.upenn.edu',
-    'Zhihang311@',
-    (success, id, error) => {
-      expect(success).toBe(true);
-      expect(id).toBe('objectID');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPost(`${rootURL}/loginUserWithEmail`).reply(201, { success: true, id: 'objectID', error: null });
+    await api.loginUserWithEmail(
+      'zhyuan1@seas.upenn.edu',
+      'Zhihang311@',
+      (success, id, error) => {
+        expect(success).toBe(true);
+        expect(id).toBe('objectID');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('modifyUser modifies an user information', async () => {
   // mock the axios response- new user 201
-  mock.onPut(`${rootURL}/modify`).reply(201, { success: true, error: null });
-  await api.modifyUser(
-    'ObjectID',
-    'password',
-    'newValue',
-    'oldPassword',
-    (success, error) => {
-      expect(success).toBe(true);
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onPut(`${rootURL}/modify`).reply(201, { success: true, error: null });
+    await api.modifyUser(
+      'ObjectID',
+      'password',
+      'newValue',
+      'oldPassword',
+      (success, error) => {
+        expect(success).toBe(true);
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('getPassword returns an user password', async () => {
   // mock the axios response- new user 201
-  mock.onGet(`${rootURL}/getPassword?id=ObjectID`).reply(201, { success: true, data: 'password', error: null });
-  await api.getPassword(
-    'ObjectID',
-    (success, data, error) => {
-      expect(success).toBe(true);
-      expect(data).toBe('password');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onGet(`${rootURL}/getPassword?id=ObjectID`).reply(201, { success: true, data: 'password', error: null });
+    await api.getPassword(
+      'ObjectID',
+      (success, data, error) => {
+        expect(success).toBe(true);
+        expect(data).toBe('password');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 test('getUserDetails returns an user information', async () => {
   // mock the axios response- new user 201
-  mock.onGet(`${rootURL}/getUserDetails?id=ObjectID`).reply(201, { success: true, data: 'userInfo', error: null });
-  await api.getUserDetails(
-    'ObjectID',
-    (success, data, error) => {
-      expect(success).toBe(true);
-      expect(data).toBe('userInfo');
-      expect(error).toBe(null);
-    },
-  );
+  try {
+    mock.onGet(`${rootURL}/getUserDetails?id=ObjectID`).reply(201, { success: true, data: 'userInfo', error: null });
+    await api.getUserDetails(
+      'ObjectID',
+      (success, data, error) => {
+        expect(success).toBe(true);
+        expect(data).toBe('userInfo');
+        expect(error).toBe(null);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
